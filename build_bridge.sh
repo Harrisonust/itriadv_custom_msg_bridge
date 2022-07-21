85327b74200b9f11eb47a70b9a6ceeefdf03d7d7
itriharrison <itri460448@itri.org.tw>
2022-07-21 07:25:34 +0000

    make auto script

--- build_bridge.sh
+++ build_bridge.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-ROS1_INSTALL_PATH=/opt/ros/noetic
-ROS2_INSTALL_PATH=/opt/ros/galactic
+
+# modify these var if needed
+ITRIADV_ROS1_INSTALL_PATH=/opt/ros/noetic
+ITRIADV_ROS2_INSTALL_PATH=/opt/ros/galactic
+THIS_DIR=$(pwd)
 ITRIADV1_DIR=/home/itri/itriadv
 ITRIADV2_DIR=/home/itri/itriadv2
 BRIDGE_DIR=/home/itri/bridge_ws
@@ -12,33 +15,44 @@ clean_all_ros_var() {
 clean_all_ros_var
 
 # build msgs under itriadv
-source $ROS1_INSTALL_PATH/setup.bash
+echo "@@@ build msgs under itriadv @@@"
+source $ITRIADV_ROS1_INSTALL_PATH/setup.bash
+printenv | grep ROS
 cd $ITRIADV1_DIR
 catkin_make --only-pkg-with-deps msgs
 
 clean_all_ros_var
 
 # build msgs under itriadv2
-source $ROS2_INSTALL_PATH/setup.bash     # setup ros2 env
-cp mapping_rules.yaml $ITRIADV2_DIR/src/msgs/
-cp CMakeLists.txt $ITRIADV2_DIR/src/msgs/
-cp package.xml $ITRIADV2_DIR/src/msgs/
+echo "@@@ build msgs under itriadv2 @@@"
+source $ITRIADV_ROS2_INSTALL_PATH/setup.bash
+printenv | grep ROS
+cp $THIS_DIR/mapping_rules.yaml $ITRIADV2_DIR/src/msgs/
+cp $THIS_DIR/CMakeLists.txt $ITRIADV2_DIR/src/msgs/
+cp $THIS_DIR/package.xml $ITRIADV2_DIR/src/msgs/
 cd $ITRIADV2_DIR
 colcon build --symlink-install --packages-select msgs
 
 clean_all_ros_var
 
 # build ros1_bridge under bridge_ws
-source $ROS1_INSTALL_PATH/setup.bash 
-source $ROS2_INSTALL_PATH/setup.bash 
+echo "@@@ build ros1_bridge under bridge_ws @@@"
+source $ITRIADV_ROS1_INSTALL_PATH/setup.bash 
+source $ITRIADV_ROS2_INSTALL_PATH/setup.bash 
 source $ITRIADV1_DIR/devel/setup.bash
 source $ITRIADV2_DIR/install/setup.bash
+rm -rf $BRIDGE_DIR/log $BRIDGE_DIR/build $BRIDGE_DIR/install 
+printenv | grep ROS
 mkdir -p $BRIDGE_DIR/src
 cd $BRIDGE_DIR/src/
-git clone git@github.com:ros2/ros1_bridge.git
+
+if [[ ! -f "$BRIDGE_DIR/src/ros1_bridge" ]]; then
+    git clone git@github.com:ros2/ros1_bridge.git
+fi
 cd $BRIDGE_DIR
 colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
 
 # Execute ros1_bridge
+echo "@@@ Execute ros1_bridge @@@"
 source $BRIDGE_DIR/install/setup.bash
 ros2 run ros1_bridge dynamic_bridge --bridge-all-topics
